b692e19a91a09f2ce2f45b9fe60ce48b1c91134d
Lunadev <vuminhdat98@gmail.com>
2024-10-03 16:44:33 +0700

    add: firstName and lastName prop

--- src/main/java/com/datvm/hairbookingapp/entity/Account.java
+++ src/main/java/com/datvm/hairbookingapp/entity/Account.java
@@ -3,10 +3,7 @@ package com.datvm.hairbookingapp.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -24,12 +21,22 @@ public class Account {
 //    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
+    @NotBlank(message = "First name can't be blank")
+    @Size(min = 2, message = "First name must be at least 2 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name can't be blank")
+    @Size(min = 2, message = "Last name must be at least 2 characters")
+    private String lastName;
+
     @Email(message = "Invalid email")
     @Column(unique = true)
     String email;
+
     @Pattern(regexp = "(84|0[3|5|7|8|9])+(\\d{8})\\b" , message = "Invalid phone number")
     @Column(unique = true)
     String phone;
+
     @Size(min = 6 , message = "Password must be exceed 6 characters ")
     String password;
 
