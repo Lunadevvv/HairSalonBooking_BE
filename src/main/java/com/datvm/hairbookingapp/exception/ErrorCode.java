e24fbbde3ca6479fb448d55daa26741b52cbdaf9
Lunadev <vuminhdat98@gmail.com>
2024-10-05 13:53:47 +0700

    complete: authorize token

--- src/main/java/com/datvm/hairbookingapp/exception/ErrorCode.java
+++ src/main/java/com/datvm/hairbookingapp/exception/ErrorCode.java
@@ -2,12 +2,12 @@ package com.datvm.hairbookingapp.exception;
 
 public enum ErrorCode {
     UNCATEGORIZE_EXCEPTION(9999, "Uncategorize exception"),
-    USER_EXISTED(1001, "User existed"),
-    PASSWORD_INVALID(1003, "Password must be at least 8 characters"),
+    TOKEN_WRONG(1001, "Token is wrong"),
+    TOKEN_EXPIRED(1003, "Token is expired"),
     DUPLICATE_EMAIL(1000,"Duplicate Email"),
     USER_NOT_EXISTED(1005, "User not existed"),
-    UNAUTHENTICATED(1006, "Unauthenticated"),
-    DUPLICATE_PHONE(1000, "Duplicat Phone"),
+    TOKEN_MISSING(1006, "Token is missing"),
+    DUPLICATE_PHONE(1000, "Duplicate Phone"),
 
     ;
 
