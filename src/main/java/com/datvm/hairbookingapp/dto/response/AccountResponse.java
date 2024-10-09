96060f6220de7065cc902e20471a8a8cf043e752
Lunadev <vuminhdat98@gmail.com>
2024-10-09 14:26:30 +0700

    add: AccountResponse Object

---
+++ src/main/java/com/datvm/hairbookingapp/dto/response/AccountResponse.java
@@ -0,0 +1,20 @@
+package com.datvm.hairbookingapp.dto.response;
+
+import com.datvm.hairbookingapp.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountResponse {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Role role;
+}
