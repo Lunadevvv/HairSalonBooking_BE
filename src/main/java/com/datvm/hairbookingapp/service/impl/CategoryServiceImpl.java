7e34e7584769b3405dde6e2ed3cbdca458024ba9
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-10 18:04:44 +0700

    Implement new method

--- src/main/java/com/datvm/hairbookingapp/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/datvm/hairbookingapp/service/impl/CategoryServiceImpl.java
@@ -1,45 +1,62 @@
 package com.datvm.hairbookingapp.service.impl;
 
+import com.datvm.hairbookingapp.dto.request.CategoryCreationRequest;
+import com.datvm.hairbookingapp.dto.request.CategoryUpdateRequest;
+import com.datvm.hairbookingapp.dto.response.CategoryResponse;
 import com.datvm.hairbookingapp.entity.Category;
+import com.datvm.hairbookingapp.exception.AppException;
+import com.datvm.hairbookingapp.mapper.CategoryMapper;
 import com.datvm.hairbookingapp.repository.CategoryRepository;
 import com.datvm.hairbookingapp.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static com.datvm.hairbookingapp.exception.ErrorCode.*;
+
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
     @Override
-    public Category createCategory(Category category) {
-        return categoryRepository.save(category);
+    public CategoryResponse createCategory(CategoryCreationRequest request) {
+        if (categoryRepository.existsById(request.getCategoryId())) throw new AppException(CATEGORY_EXISTED);
+        Category category = categoryMapper.toCategory(request);
+        // can't map from 3 entities to 4 entities
+        return categoryMapper.toCategoryResponse(categoryRepository.save(category));
     }
 
     @Override
-    public Category getCategoryById(Long id) {
-        return categoryRepository.findCategoryByCategoryId(id);
+    public CategoryResponse findCategoryById(Long id) {
+        if (!categoryRepository.existsById(id)) throw new AppException(CATEGORY_NOT_EXISTED);
+        return categoryMapper.toCategoryResponse(categoryRepository.findByCategoryId(id));
     }
 
     @Override
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+    public List<CategoryResponse> findAllCategory() {
+
+        return categoryRepository.findAll().stream().map(categoryMapper::toCategoryResponse).toList();
+
     }
 
     @Override
-    public int updateCategoryById(Long id, Category category) {
-        String name = category.getCategoryName();
-        String description = category.getCategoryDescription();
-        return categoryRepository.updateCategoryById(id,name,description);
+    public CategoryResponse updateCategoryById(Long id, CategoryUpdateRequest request) {
+        if (!categoryRepository.existsById(id)) throw new AppException(CATEGORY_NOT_EXISTED);
+        Category category = categoryRepository.findByCategoryId(id);
+        categoryMapper.updateCategory(category, request);
+        return categoryMapper.toCategoryResponse(categoryRepository.save(category));
     }
 
     @Override
     public void deleteCategoryById(Long id) {
-        categoryRepository.deleteCategoryByCategoryId(id);
+        if (!categoryRepository.existsById(id)) throw new AppException(CATEGORY_NOT_EXISTED);
+        categoryRepository.deleteByCategoryId(id);
     }
 
-    @Override
-    public List<Category> getCategoryByName(String name) {
-        return categoryRepository.findByCategoryNameContaining(name);
-    }
 }
