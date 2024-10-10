ba3f134ecafe30182648aec13bb768e0c9f62d68
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-10 21:58:09 +0700

    Create ServicesResponse

---
+++ src/main/java/com/datvm/hairbookingapp/dto/response/ServicesResponse.java
@@ -0,0 +1,21 @@
+package com.datvm.hairbookingapp.dto.response;
+
+import com.datvm.hairbookingapp.entity.Category;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ServicesResponse {
+    Long serviceId;
+    String serviceName;
+    String description;
+    String duration;
+    BigDecimal price;
+    Category categories;
+}
