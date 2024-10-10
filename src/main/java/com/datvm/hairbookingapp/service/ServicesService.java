ac49b479e69023aaacb0e2d8fd845b5a15c258dc
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-10 21:55:01 +0700

    Change method name and type ServicesService

--- src/main/java/com/datvm/hairbookingapp/service/ServicesService.java
+++ src/main/java/com/datvm/hairbookingapp/service/ServicesService.java
@@ -1,15 +1,18 @@
 package com.datvm.hairbookingapp.service;
 
+import com.datvm.hairbookingapp.dto.request.ServicesCreationRequest;
+import com.datvm.hairbookingapp.dto.request.ServicesUpdateRequest;
+import com.datvm.hairbookingapp.dto.response.ApiResponse;
+import com.datvm.hairbookingapp.dto.response.ServicesResponse;
 import com.datvm.hairbookingapp.entity.Services;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.List;
 @Service
 public interface ServicesService {
-    Services createService(Services services, Long categoryId);
-    List<Services> getAllServices();
-    int updateServicesByServiceId(Services service, Long categoryId, Long serviceId);
-    void deleteServicesByServiceId(Long id);
-    Services findServicesByServiceId(Long id);
+    ServicesResponse createService(ServicesCreationRequest request, Long categoryId);
+    ServicesResponse findByServiceId(Long serviceId);
+    List<ServicesResponse> findAllServices();
+    ServicesResponse updateByServiceId(ServicesUpdateRequest request, Long ServiceId, Long categoryId);
+    void deleteByServiceId(Long serviceId);
+
 }
