a8ecb1505b628d659d81e85636b4cf69da4fdf91
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-11 23:47:15 +0700

    create and add constraint for String image

--- src/main/java/com/datvm/hairbookingapp/entity/Services.java
+++ src/main/java/com/datvm/hairbookingapp/entity/Services.java
@@ -11,16 +11,22 @@ import java.math.BigDecimal;
 public class Services {
     @Id
     private Long serviceId;
+
     @Column(nullable = false)
     private String serviceName;
+
     @Column(nullable = false)
     private String description;
+
     @Column(nullable = false)
     private String duration;
+
     @Column(nullable = false)
     private BigDecimal price;
 
+    @Column(nullable = false)
     private String image;
+
     @ManyToOne(targetEntity = Category.class,fetch = FetchType.EAGER)
     @JoinColumn(name = "CategoryId", nullable = false)
     private Category categories;
