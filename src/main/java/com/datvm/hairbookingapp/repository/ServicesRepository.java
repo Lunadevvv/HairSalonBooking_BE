af05494621918e49bed105ee4e5ad3849ca685d4
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-12 19:45:35 +0700

    change long primary key to String add findLastId method

--- src/main/java/com/datvm/hairbookingapp/repository/ServicesRepository.java
+++ src/main/java/com/datvm/hairbookingapp/repository/ServicesRepository.java
@@ -4,14 +4,17 @@ import com.datvm.hairbookingapp.entity.Services;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ServicesRepository extends JpaRepository<Services, Long>, JpaSpecificationExecutor<Services> {
+public interface ServicesRepository extends JpaRepository<Services, String>, JpaSpecificationExecutor<Services> {
     List<Services> findAll();
-    Services findByServiceId(Long id);
+    Services findByServiceId(String id);
     @Transactional
-    void deleteByServiceId(Long serviceId);
+    void deleteByServiceId(String serviceId);
 
+    @Query("SELECT MAX(s.serviceId) FROM Services s")
+    String findLastId();
 }
