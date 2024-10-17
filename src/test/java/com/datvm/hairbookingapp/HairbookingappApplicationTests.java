8b47071ba5bccdf397c10bd9c3734742f9540677
Lunadev <vuminhdat98@gmail.com>
2024-10-17 23:43:11 +0700

    updated: test function

--- src/test/java/com/datvm/hairbookingapp/HairbookingappApplicationTests.java
+++ src/test/java/com/datvm/hairbookingapp/HairbookingappApplicationTests.java
@@ -1,13 +1,26 @@
 package com.datvm.hairbookingapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
+@Slf4j
 @SpringBootTest
 class HairbookingappApplicationTests {
 
+//	@Test
+//	void contextLoads() {
+//	}
+
 	@Test
-	void contextLoads() {
+	void testDate(){
+
+		LocalDate today = LocalDate.now();
+
+		// Print today's date
+		log.info("Today's date: " + today);
 	}
 
 }
--- src/test/java/com/datvm/hairbookingapp/entity/CategoryTest.java
+++ src/test/java/com/datvm/hairbookingapp/entity/CategoryTest.java
@@ -15,30 +15,30 @@ public class CategoryTest {
     @Transactional
     @Rollback(false)
     void addTest(){
-        Category category = new Category();
-
-        category.setCategoryId(01L);
-        category.setCategoryName("Haircut");
-        category.setCategoryDescription("Cắt tóc");
-
-        Category category2 = new Category();
-        category2.setCategoryId(02L);
-        category2.setCategoryName("Hair Styling");
-        category2.setCategoryDescription("tạo mẫu");
-
-        Category category3 = new Category();
-        category3.setCategoryId(03L);
-        category3.setCategoryName("Hair Care");
-        category3.setCategoryDescription("Gội đầu");
-
-        Category category4 = new Category();
-        category4.setCategoryId(04L);
-        category4.setCategoryName("Massage");
-        category4.setCategoryDescription("Mát xa");
-
-        categoryRepository.save(category);
-        categoryRepository.save(category2);
-        categoryRepository.save(category3);
-        categoryRepository.save(category4);
+//        Category category = new Category();
+//
+//        category.setCategoryId(01L);
+//        category.setCategoryName("Haircut");
+//        category.setCategoryDescription("Cắt tóc");
+//
+//        Category category2 = new Category();
+//        category2.setCategoryId(02L);
+//        category2.setCategoryName("Hair Styling");
+//        category2.setCategoryDescription("tạo mẫu");
+//
+//        Category category3 = new Category();
+//        category3.setCategoryId(03L);
+//        category3.setCategoryName("Hair Care");
+//        category3.setCategoryDescription("Gội đầu");
+//
+//        Category category4 = new Category();
+//        category4.setCategoryId(04L);
+//        category4.setCategoryName("Massage");
+//        category4.setCategoryDescription("Mát xa");
+//
+//        categoryRepository.save(category);
+//        categoryRepository.save(category2);
+//        categoryRepository.save(category3);
+//        categoryRepository.save(category4);
     }
 }
