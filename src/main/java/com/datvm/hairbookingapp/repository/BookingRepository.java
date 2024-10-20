ee74c1bc68f4ed5682bc4f69defa5d970b5720d8
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-20 19:24:35 +0700

    add countBookingInSlot method

--- src/main/java/com/datvm/hairbookingapp/repository/BookingRepository.java
+++ src/main/java/com/datvm/hairbookingapp/repository/BookingRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, String> {
     @Query("SELECT MAX(b.id) FROM Booking b")
@@ -14,6 +16,10 @@ public interface BookingRepository extends JpaRepository<Booking, String> {
 
     @Query("SELECT b FROM Booking b WHERE b.account = :account ORDER BY b.id DESC LIMIT 1 ")
     Booking findLastBooking(@Param("account")Account account);
+
+    @Query("SELECT COUNT(b) FROM Booking b WHERE b.date = ?1 AND b.slot.id = ?2")
+    int countBookingInSlot(LocalDate date, Long slotId);
+
 
 //    Booking findTopByAccountOrderByDateDesc(Account account);
 }
