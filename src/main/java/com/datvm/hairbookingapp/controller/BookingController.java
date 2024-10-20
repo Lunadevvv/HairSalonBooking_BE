bdc96e4ccdf2a53c4b351f0ab6ed889fcd11bbd3
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-20 21:48:46 +0700

    Add temporary getAllBookings Add CancelPeriodBooking

--- src/main/java/com/datvm/hairbookingapp/controller/BookingController.java
+++ src/main/java/com/datvm/hairbookingapp/controller/BookingController.java
@@ -4,17 +4,27 @@ import com.datvm.hairbookingapp.dto.request.BookingRequest;
 import com.datvm.hairbookingapp.dto.request.BookingUpdateRequest;
 import com.datvm.hairbookingapp.dto.response.ApiResponse;
 import com.datvm.hairbookingapp.dto.response.BookingResponse;
+import com.datvm.hairbookingapp.entity.Booking;
 import com.datvm.hairbookingapp.entity.enums.BookingStatus;
 import com.datvm.hairbookingapp.service.BookingService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/booking")
 public class BookingController {
     @Autowired
     BookingService bookingService;
+
+    @GetMapping
+    public ApiResponse<List<Booking>> getAllBookings() {
+        return ApiResponse.<List<Booking>>builder()
+                .result(bookingService.getBookings())
+                .build();
+    }
 
     @PostMapping
     public ApiResponse<BookingResponse> createBooking(@RequestBody @Valid BookingRequest request){
@@ -33,6 +43,7 @@ public class BookingController {
                 .message("Update thành công booking " + id + " thành " + status.toString())
                 .build();
     }
+
     @PutMapping()
     public ApiResponse<BookingResponse> updateBookingSlot(@RequestBody @Valid BookingUpdateRequest request){
         return ApiResponse.<BookingResponse>builder()
@@ -40,6 +51,15 @@ public class BookingController {
                 .result(bookingService.updateBookingSlot(request))
                 .build();
     }
+
+    @PutMapping("/{id}")
+    public ApiResponse<BookingResponse> CancelPeriodBooking(@PathVariable String id){
+        return ApiResponse.<BookingResponse>builder()
+                .message("Cancel thành công")
+                .result(bookingService.cancelPeriodBooking(id))
+                .build();
+    }
+
     @DeleteMapping("/{id}")
     public ApiResponse<String> deleteBooking(@PathVariable String id){
         return ApiResponse.<String>builder()
