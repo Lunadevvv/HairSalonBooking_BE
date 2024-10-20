283e977f4d0ea69365eee693d1fd805ee8a9dc8a
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-20 19:23:22 +0700

    delete bug

--- src/main/java/com/datvm/hairbookingapp/mapper/BookingMapper.java
+++ src/main/java/com/datvm/hairbookingapp/mapper/BookingMapper.java
@@ -1,10 +1,12 @@
 package com.datvm.hairbookingapp.mapper;
 
+import com.datvm.hairbookingapp.dto.request.CategoryUpdateRequest;
 import com.datvm.hairbookingapp.dto.response.BookingResponse;
 import com.datvm.hairbookingapp.entity.Booking;
+import com.datvm.hairbookingapp.entity.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper
 public interface BookingMapper {
-    BookingResponse toRes(Booking booking);
 }
