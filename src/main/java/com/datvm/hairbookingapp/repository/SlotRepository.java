db227d612d94ebfb2ef37c0eccd798f738d9890a
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-21 11:27:02 +0700

    add findByTimeStart in SlotRepository

--- src/main/java/com/datvm/hairbookingapp/repository/SlotRepository.java
+++ src/main/java/com/datvm/hairbookingapp/repository/SlotRepository.java
@@ -2,8 +2,13 @@ package com.datvm.hairbookingapp.repository;
 
 import com.datvm.hairbookingapp.entity.Slot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalTime;
 
 @Repository
 public interface SlotRepository extends JpaRepository<Slot, Long> {
+    @Query("select s.id From Slot s where s.timeStart = ?1")
+    Long findByTimeStart(LocalTime timeStart);
 }
