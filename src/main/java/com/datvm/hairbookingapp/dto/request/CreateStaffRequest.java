9e2ccbe06b61eee8d13ad06fe9ada903cf706117
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-23 18:36:28 +0700

    add salonId to CreateStaffRequest

--- src/main/java/com/datvm/hairbookingapp/dto/request/CreateStaffRequest.java
+++ src/main/java/com/datvm/hairbookingapp/dto/request/CreateStaffRequest.java
@@ -2,6 +2,7 @@ package com.datvm.hairbookingapp.dto.request;
 
 import com.datvm.hairbookingapp.entity.enums.Role;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
@@ -40,4 +41,7 @@ public class CreateStaffRequest {
 
     @Pattern(regexp = "https://imgur\\.com.*", message = "Invalid url. Default: https://imgur.com")
     private String image;
+
+    @NotBlank(message = "salonId can't be blank")
+    private String salonId;
 }
