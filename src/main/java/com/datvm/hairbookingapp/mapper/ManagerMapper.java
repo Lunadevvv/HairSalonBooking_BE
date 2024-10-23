442d86ff444262a7f3e762a19fc5a5a807a1a115
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-23 23:00:37 +0700

    fix ManagerMapper bug

--- src/main/java/com/datvm/hairbookingapp/mapper/ManagerMapper.java
+++ src/main/java/com/datvm/hairbookingapp/mapper/ManagerMapper.java
@@ -2,7 +2,6 @@ package com.datvm.hairbookingapp.mapper;
 
 import com.datvm.hairbookingapp.dto.response.ManagerResponse;
 import com.datvm.hairbookingapp.entity.Manager;
-import com.datvm.hairbookingapp.repository.ManagerRepository;
 import org.mapstruct.Mapper;
 
 @Mapper
