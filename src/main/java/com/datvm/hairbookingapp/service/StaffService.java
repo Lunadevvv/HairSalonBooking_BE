f6579189b015f701589b8b6ee2c15def512595d8
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-23 18:47:49 +0700

    modify createStaff in StaffService to set staff salon

--- src/main/java/com/datvm/hairbookingapp/service/StaffService.java
+++ src/main/java/com/datvm/hairbookingapp/service/StaffService.java
@@ -4,12 +4,14 @@ import com.datvm.hairbookingapp.dto.request.CreateStaffRequest;
 import com.datvm.hairbookingapp.dto.request.DateAndSlotRequest;
 import com.datvm.hairbookingapp.dto.response.StaffResponse;
 import com.datvm.hairbookingapp.entity.Account;
+import com.datvm.hairbookingapp.entity.Salon;
 import com.datvm.hairbookingapp.entity.enums.Role;
 import com.datvm.hairbookingapp.entity.Staff;
 import com.datvm.hairbookingapp.exception.AppException;
 import com.datvm.hairbookingapp.exception.ErrorCode;
 import com.datvm.hairbookingapp.mapper.AccountMapper;
 import com.datvm.hairbookingapp.repository.AuthenticationRepository;
+import com.datvm.hairbookingapp.repository.SalonRepository;
 import com.datvm.hairbookingapp.repository.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -34,69 +36,76 @@ public class StaffService {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    public List<StaffResponse> getAvailableStylist(DateAndSlotRequest request){
+    @Autowired
+    SalonRepository salonRepository;
+
+    public List<StaffResponse> getAvailableStylist(DateAndSlotRequest request) {
         List<Staff> list = staffRepository.findAvailableStylists(request.getSlotId(), request.getDate(), Role.STYLIST);
         return list.stream().map(accountMapper::toStaffRes).collect(Collectors.toList());
     }
 
-    public void deleteStaff(String code){
+    public void deleteStaff(String code) {
         Staff staff = staffRepository.findStaffByCode(code);
-        if(staff == null)
+        if (staff == null)
             throw new AppException(ErrorCode.STAFF_NOT_FOUND);
         Account account = authenticationRepository.findAccountByPhone(staff.getPhone());
         try {
             staffRepository.delete(staff);
             authenticationRepository.delete(account);
-        }catch (AppException e){
+        } catch (AppException e) {
             throw new AppException(ErrorCode.PROCESS_FAILED);
         }
     }
 
-    public List<Staff> getAllStaff(){
+    public List<Staff> getAllStaff() {
         List<Staff> list = staffRepository.findAll();
         return list;
     }
 
-    public StaffResponse getStaffByCode(String code){
+    public StaffResponse getStaffByCode(String code) {
         Staff staff = staffRepository.findStaffByCode(code);
-        if(staff == null)
+        if (staff == null)
             throw new AppException(ErrorCode.STAFF_NOT_FOUND);
         return accountMapper.toStaffRes(staff);
     }
 
     public StaffResponse createStaff(CreateStaffRequest request) throws HttpMessageNotReadableException {
-            String code = generateStaffCode();
-            if(request.getRole() == null)
-                request.setRole(Role.STAFF);
-            Staff staff = accountMapper.toStaff(request);
-            staff.setCode(code);
-            staff.setImage(request.getImage());
-            staff.setRole(request.getRole());
-            try{
-                staff.setAccount(authenticationRepository.save(Account.builder()
-                        .email(staff.getEmail())
-                        .staff(staff)
-                        .role(request.getRole())
-                        .phone(staff.getPhone())
-                        .lastName(staff.getLastName())
-                        .firstName(staff.getFirstName())
-                        .password(passwordEncoder.encode("staff123"))
-                        .build()
-                ));
-                staff = staffRepository.save(staff);
-            }catch (AppException e){
-                throw new AppException(ErrorCode.PROCESS_FAILED);
-            }
-
-            StaffResponse res = accountMapper.toStaffRes(staff);
-            res.setImage(staff.getImage());
-            return res;
+        Salon salon = salonRepository.findById(request.getSalonId()).orElseThrow( () -> new AppException(ErrorCode.SALON_NOT_FOUND));
+        String code = generateStaffCode();
+        if (request.getRole() == null)
+            request.setRole(Role.STAFF);
+        Staff staff = accountMapper.toStaff(request);
+        staff.setCode(code);
+        staff.setImage(request.getImage());
+        staff.setRole(request.getRole());
+        staff.setSalons(salon);
+        try {
+            staff.setAccount(authenticationRepository.save(Account.builder()
+                    .email(staff.getEmail())
+                    .staff(staff)
+                    .role(request.getRole())
+                    .phone(staff.getPhone())
+                    .lastName(staff.getLastName())
+                    .firstName(staff.getFirstName())
+                    .password(passwordEncoder.encode("staff123"))
+                    .build()
+            ));
+            staff = staffRepository.save(staff);
+        } catch (AppException e) {
+            throw new AppException(ErrorCode.PROCESS_FAILED);
+        }
+        StaffResponse res = accountMapper.toStaffRes(staff);
+        res.setImage(staff.getImage());
+        res.setSalons(staff.getSalons());
+        return res;
     }
 
-    public StaffResponse updateStaffProfile(CreateStaffRequest request, String code){
+
+    public StaffResponse updateStaffProfile(CreateStaffRequest request, String code) {
         Staff staff = staffRepository.findStaffByCode(code);
-        if(staff == null)
+        if (staff == null)
             throw new AppException(ErrorCode.STAFF_NOT_FOUND);
+        Salon salon = salonRepository.findById(request.getSalonId()).orElseThrow( () -> new AppException(ErrorCode.SALON_NOT_FOUND));
         staff.setGender(request.getGender());
         staff.setEmail(request.getEmail());
         staff.setYob(request.getYob());
@@ -106,7 +115,7 @@ public class StaffService {
         staff.setJoinIn(request.getJoinIn());
         staff.setImage(request.getImage());
         staff.setRole(request.getRole());
-
+        staff.setSalons(salon);
         Account account = staff.getAccount();
         if (account != null) {
             account.setFirstName(request.getFirstName());
@@ -118,17 +127,17 @@ public class StaffService {
             }
         }
 
-        try{
+        try {
             return accountMapper.toStaffRes(staffRepository.save(staff));
-        }catch (AppException e) {
+        } catch (AppException e) {
             throw new AppException(ErrorCode.PROCESS_FAILED);
         }
     }
 
-    public String generateStaffCode(){
+    public String generateStaffCode() {
         String code = "S0001";
         String latestCode = staffRepository.findTheLatestStaffCode();
-        if(latestCode == null)
+        if (latestCode == null)
             return code;
         int fourLastChar = Integer.parseInt(latestCode.substring(1));
         code = String.format("S%04d", ++fourLastChar);
