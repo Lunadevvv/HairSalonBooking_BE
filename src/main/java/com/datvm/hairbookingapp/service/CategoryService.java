cf647d8452035406dfa01549daff84cdd3268799
TonyJugo <nhatchse184713@fpt.edu.vn>
2024-10-26 15:07:48 +0700

    xóa khỏi database, set  tất cả service của category đó  thành null

--- src/main/java/com/datvm/hairbookingapp/service/CategoryService.java
+++ src/main/java/com/datvm/hairbookingapp/service/CategoryService.java
@@ -1,13 +1,14 @@
-package com.datvm.hairbookingapp.service.impl;
+package com.datvm.hairbookingapp.service;
 
 import com.datvm.hairbookingapp.dto.request.CategoryCreationRequest;
 import com.datvm.hairbookingapp.dto.request.CategoryUpdateRequest;
 import com.datvm.hairbookingapp.dto.response.CategoryResponse;
 import com.datvm.hairbookingapp.entity.Category;
+import com.datvm.hairbookingapp.entity.Services;
 import com.datvm.hairbookingapp.exception.AppException;
 import com.datvm.hairbookingapp.mapper.CategoryMapper;
 import com.datvm.hairbookingapp.repository.CategoryRepository;
-import com.datvm.hairbookingapp.service.interfaces.CategoryService;
+import com.datvm.hairbookingapp.repository.ServicesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +18,15 @@ import static com.datvm.hairbookingapp.exception.ErrorCode.*;
 
 
 @Service
-public class CategoryServiceImpl implements CategoryService {
+public class CategoryService  {
     @Autowired
     private CategoryRepository categoryRepository;
 
     @Autowired
     private CategoryMapper categoryMapper;
+    @Autowired
+    private ServicesRepository servicesRepository;
 
-    @Override
     public CategoryResponse createCategory(CategoryCreationRequest request) {
         Category category = categoryMapper.toCategory(request);
         // can't map from 3 entities to 4 entities
@@ -32,20 +34,17 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryMapper.toCategoryResponse(categoryRepository.save(category));
     }
 
-    @Override
     public CategoryResponse findCategoryById(String id) {
         if (!categoryRepository.existsById(id)) throw new AppException(CATEGORY_NOT_EXISTED);
         return categoryMapper.toCategoryResponse(categoryRepository.findByCategoryId(id));
     }
 
-    @Override
     public List<CategoryResponse> findAllCategory() {
 
         return categoryRepository.findAll().stream().map(categoryMapper::toCategoryResponse).toList();
 
     }
 
-    @Override
     public CategoryResponse updateCategoryById(CategoryUpdateRequest request) {
         if (!categoryRepository.existsById(request.getCategoryId())) throw new AppException(CATEGORY_NOT_EXISTED);
         Category category = categoryRepository.findByCategoryId(request.getCategoryId());
@@ -53,13 +52,19 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryMapper.toCategoryResponse(categoryRepository.save(category));
     }
 
-    @Override
     public void deleteCategoryById(String id) {
         if (!categoryRepository.existsById(id)) throw new AppException(CATEGORY_NOT_EXISTED);
+        Category category = categoryRepository.findByCategoryId(id);
+        List<Services> services = category.getService();
+        for (Services service : services) {
+            service.setCategories(null);
+        }
+        category.setService(null);
+        categoryRepository.save(category);
         categoryRepository.deleteByCategoryId(id);
     }
 
-    @Override
+
     public String generateCategoryId() {
         String id = "CT0001";
         String lastId = categoryRepository.findLastId();
